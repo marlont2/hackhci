1dd90004c89b22458b84b2f1eaff7612f8cd168b
OpenShift guest <marlont2@u.northwestern.edu>
2013-10-25 16:45:07 -0400

    Added a new function.

--- Week1.js
+++ Week1.js
@@ -1,6 +1,26 @@
 function calculate(){
         var input = $('#expression'); //get the box
         var val = input.val(); //get the value inside the box
-        console.log(val.toUpperCase()); //log the value to the console
+        // Here is the actual calculation
+        var pattern = /\d+|\+|\-|\*|\/|\(|\)/g
+        var tokens = val.match(pattern);
+        
+        var output = $('#output'); // get the output from input box
+        output.text(JSON.stringify(tokens));
     }
+    
+function read_operand(tokens){
+        var num = tokens[0];
+        var token_shift = tokens.shift()
+        var numint = parseInt(num,10)
+        if(numint==NaN){
+            throw "number expected"
+        }else{
+            return numint
+        }
+    }
+    
+    
+        
+
         
