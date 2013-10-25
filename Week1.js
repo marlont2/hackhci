692e230aa71a882bef0d84f9184548020cc194b0
OpenShift guest <marlont2@u.northwestern.edu>
2013-10-25 15:42:54 -0400

    ^x ^X
    Javascript File

---
+++ Week1.js
@@ -0,0 +1,6 @@
+function calculate(){
+        var input = $('#expression'); //get the box
+        var val = input.val(); //get the value inside the box
+        console.log(val.toUpperCase()); //log the value to the console
+    }
+        
