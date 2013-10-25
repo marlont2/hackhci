6fbe75ba174274d8fbe1cae48e4bd48e735cdd18
OpenShift guest <marlont2@u.northwestern.edu>
2013-10-25 17:13:06 -0400

    Function for Evaluation

--- Week1.js
+++ Week1.js
@@ -1,26 +1,31 @@
 function calculate(){
-        var input = $('#expression'); //get the box
-        var val = input.val(); //get the value inside the box
-        // Here is the actual calculation
-        var pattern = /\d+|\+|\-|\*|\/|\(|\)/g
-        var tokens = val.match(pattern);
-        
-        var output = $('#output'); // get the output from input box
-        output.text(JSON.stringify(tokens));
-    }
+    var input = $('#expression'); //get the box
+    var val = input.val(); //get the value inside the box
+    // Here is the actual calculation
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g //The pattern matching
+    var tokens = val.match(pattern);
+    var output = $('#output'); // get the output from input box
+    output.text(JSON.stringify(tokens));
+}
     
 function read_operand(tokens){
-        var num = tokens[0];
-        var token_shift = tokens.shift()
-        var numint = parseInt(num,10)
-        if(numint==NaN){
-            throw "number expected"
-        }else{
-            return numint
-        }
+    var num = tokens[0];
+    var token_shift = tokens.shift()
+    var numint = parseInt(num,10)
+    if(isNaN(numint)){
+        throw "number expected"
+    }else{
+        return numint
     }
+}
     
+function evaluate(tokens){
+    if(tokens.length < 1){
+        throw "missing operand"
+    }
+    var val = read_operand(tokens)
+    while(tokens.length > 1){
+        
+    }
+}
     
-        
-
-        
