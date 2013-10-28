8160b4895e9e844328ee37e9afed9ba72f5faafc
OpenShift guest <marlont2@u.northwestern.edu>
2013-10-28 15:40:40 -0400

    Evaluate function is complete

--- Week1.js
+++ Week1.js
@@ -8,35 +8,44 @@ function calculate(){
     var input = $('#expression'); //get the box
     var val = input.val(); //get the value inside the box
     // Here is the actual calculation
-    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g //The pattern matching
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g; //The pattern matching
     var tokens = val.match(pattern); //Finds the operators in the value
     var output = $('#output'); // get the output from input box
     output.text(JSON.stringify(tokens));
 }
     
 function read_operand(tokens){
-    try{
-    var num = tokens[0]; //num is set to the first array element
-    var token_shift = tokens.shift() //the first element is removed from array
-    var numint = parseInt(num,10) //The variable for an integer in the array
+    try{ 
+    var num = tokens.shift(); //num is set to the first element and removed from array
+    var numint = parseInt(num,10); //The variable for an integer in the array
     if(isNaN(numint)){
-        throw "number expected"
+        throw "number expected";
     }else{
-        return [numint,tokens]
+        return numint;
     }
     }
     catch(err){
-        return err
+        return err;
     }
     }
     
 function evaluate(tokens){
-    if(tokens.length < 1){
-        throw "missing operand"
+    if(tokens.length === 0){
+        throw "missing operand";
     }
-    var val_array = read_operand(tokens)
-    while(tokens.length > 1){
-        
+    var val = read_operand(tokens);
+    while(tokens.length > 0){
+        var operator = tokens.shift();
+        if((operator!="+")&&(operator!="-")&&(operator!="*")&&(operator!="/")){
+            throw "unrecognized operator";
+        }else if(tokens.length === 0){throw "missing operand"}
+        var temp = read_operand(tokens);
+        if(operator=="+"){val = val + temp}
+        else if(operator=="-"){val = val - temp}
+        else if(operator=="*"){val = val * temp}
+        else if(operator=="/"){val = val / temp}
+    }    
+    return val;
     }
-}
+
     
